737a396333c07a21ac661414f14eddd08dedcbfc
BalanGabriela05 <gabry.balan05@yahoo.com>
2024-05-27 23:30:12 +0300

    upp sett

--- ClientApplication/src/main/java/prepareShips/SettingsPlaceShip.java
+++ ClientApplication/src/main/java/prepareShips/SettingsPlaceShip.java
@@ -1,6 +1,8 @@
 package prepareShips;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.concurrent.Semaphore;
 
@@ -16,9 +18,10 @@ public class SettingsPlaceShip extends JPanel {
     JSpinner fromColumn;
     JSpinner toRow;
     JSpinner toColumn;
+    JLabel serverResponse;
     JPanel messagePanel = new JPanel(new BorderLayout());
     // afisarea mesajului de la server dupa apasarea butonului
-    JLabel messageLabel = new JLabel();
+    JTextArea messageTextArea;
 
     public SettingsPlaceShip(MainFrame frame) {
         this.frame = frame;
@@ -63,67 +66,81 @@ public class SettingsPlaceShip extends JPanel {
         textSizeShip.setFont(newFont);
         textNameShip.setFont(newFont);
 
+        serverResponse = new JLabel("Message : ");
+        serverResponse.setFont(newFont);
+
         // Creare panouri pentru a grupa labelul și spinerele
         JPanel fromPanel = new JPanel();
         fromPanel.setLayout(new FlowLayout());
         fromPanel.add(placeShipFrom);
         fromPanel.add(fromRow);
         fromPanel.add(fromColumn);
+        fromPanel.setBorder(new EmptyBorder(0, 0, 0, 100));
 
         JPanel toPanel = new JPanel();
         toPanel.setLayout(new FlowLayout());
         toPanel.add(placeShipTo);
         toPanel.add(toRow);
         toPanel.add(toColumn);
+        toPanel.setBorder(new EmptyBorder(0,0,0,100));
 
         JPanel namePanel = new JPanel();
         namePanel.setLayout(new FlowLayout());
         namePanel.add(nameShip);
         namePanel.add(textNameShip);
+        namePanel.setBorder(new EmptyBorder(0,0,0,100));
 
         JPanel sizePanel = new JPanel();
         sizePanel.setLayout(new FlowLayout());
         sizePanel.add(sizeShip);
         sizePanel.add(textSizeShip);
+        sizePanel.setBorder(new EmptyBorder(0,0,0,100));
 
         // Setare layout pentru acest JPanel ca BoxLayout pe verticală
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         // Adăugare panouri la acest JPanel
+        add(Box.createVerticalStrut(20));
         add(namePanel);
-        add(Box.createVerticalStrut(0)); // Adăugați un strut vertical între panouri
+        add(Box.createVerticalStrut(4)); // Adăugați un strut vertical între panouri
         add(sizePanel);
-        add(Box.createVerticalStrut(0)); // Adăugați un strut vertical între panouri
+        add(Box.createVerticalStrut(40)); // Adăugați un strut vertical între panouri
         add(fromPanel);
-        add(Box.createVerticalStrut(0)); // Adăugați un strut vertical între panouri
+        add(Box.createVerticalStrut(4)); // Adăugați un strut vertical între panouri
         add(toPanel);
+        add(Box.createVerticalStrut(80));
+        add(serverResponse);
+        serverResponse.setBorder(new EmptyBorder(0,0,0,100));
+        add(Box.createVerticalStrut(4));
+
 
         //mesaj de la server
+        messagePanel.setBorder(new EmptyBorder(0,0,0,100));// nu vreau ca jpanelul sa fie lipit de margine dar nu functioneaza
+        messagePanel.setBorder(new LineBorder(new Color(0, 0, 0, 123), 10));// ca textul sa nu fie lipit de margine
+
         messagePanel.setBackground(new Color(0, 0, 0, 123));//setare backgorund
-        messageLabel.setForeground(Color.WHITE); // Setează culoarea textului la alb
-        messageLabel.setHorizontalAlignment(JLabel.CENTER); // Aliniază textul la centru
-//        messagePanel.setPreferredSize(new Dimension(messagePanel.getPreferredSize().width, 10)); //nu merge
+
+// Crearea unui nou JTextArea
+        messageTextArea = new JTextArea();
+        messageTextArea.setWrapStyleWord(true); //la sfarsitul cuv daca e nevoie trece la randul urm
+        messageTextArea.setLineWrap(true); //cand ajunge la capat trece la urm rand
+        messageTextArea.setEditable(false); //nu poate fi modificat
+        messageTextArea.setForeground(Color.WHITE);
+        messageTextArea.setBackground(new Color(0, 0, 0, 123));
+
+// Adăugați textul primit de la server
+        String text = "Response Serverdrhgrdhbbbfd bfhdtht dgnhxjyjyymjhxj ymgmgymtgynmgjmyjmgy";
+        messageTextArea.setText(text);
+
+// Adăugarea JTextArea la JPanel
+        messagePanel.add(messageTextArea, BorderLayout.CENTER);
 
 
-        // Adăugarea JLabel la JPanel
-        messagePanel.add(messageLabel, BorderLayout.CENTER);
-
-        // Adăugarea JPanel la JFrame
+// Adăugarea JPanel la JFrame
         add(messagePanel);
+        add(Box.createVerticalStrut(70));
 
 
-//mesaj de la server
-        messagePanel.setBackground(new Color(0, 0, 0, 123));//setare backgorund
-        messageLabel.setForeground(Color.WHITE); // Setează culoarea textului la alb
-        messageLabel.setHorizontalAlignment(JLabel.CENTER); // Aliniază textul la centru
-//        messagePanel.setPreferredSize(new Dimension(messagePanel.getPreferredSize().width, 10)); //nu merge
-
-
-        // Adăugarea JLabel la JPanel
-        messagePanel.add(messageLabel, BorderLayout.CENTER);
-
-        // Adăugarea JPanel la JFrame
-        add(messagePanel);
     }
 
     private String getMessage() {
